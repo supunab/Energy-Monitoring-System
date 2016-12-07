5e7cf450c3c9f7e1ee086589103d5714e78efb7a
Prabod Rathnayaka <prabod@rathnayaka.me>
2016-12-08 03:33:39 +0530

    Insert defined

--- model/User.js
+++ model/User.js
@@ -2,6 +2,7 @@
  * Created by prabod on 12/7/16.
  */
 import * as field from '../orm/Fields'
+import * as ORM from '../orm/orm'
 
 class User{
     constructor(){
@@ -10,13 +11,19 @@ class User{
         this.user_name = new field.CharField(20);
         this.password = new field.CharField(30);
         this.is_admin = new field.BooleanField();
+
     }
 
-    create(first_name, last_name, user_name, password,is_admin){
+    createObject(first_name, last_name, user_name, password,is_admin){
         this.first_name.set(first_name);
         this.last_name.set(last_name);
         this.user_name.set(user_name);
         this.password.set(password);
         this.is_admin.set(is_admin);
+    }
+
+    insert(){
+        let orm = new ORM();
+        orm.insert(this);
     }
 }
--- orm/orm.js
+++ orm/orm.js
@@ -2,18 +2,28 @@
  * Created by prabod on 12/7/16.
  */
 
-var mysql      = require('mysql');
-function orm(host, user, password, database) {
-    var connection = mysql.createConnection({
-        host     : host,
-        user     : user,
-        password : password,
-        database : database
-    });
+const mysql      = require('mysql');
+export class orm{
+    constructor(host, user, password, database) {
+        this.connection = mysql.createConnection({
+            host: host,
+            user: user,
+            password: password,
+            database: database
+        });
+        this.connection.connect();
+    }
 
-    connection.connect();
+    insert(model){
+        let table = model.constructor.name;
+        let values = [];
+        for(let key in model){
+            values.push(model[key].get())
+        }
+        this.connection.query(
+            "INSERT INTO " + table +
+            " VALUES " + "(" +
+            values.join() +")"
+        );
+    }
 }
-
-
-
-module.exports = orm;
