5d873d7c017c47e1b44d4a328732a7a0f5692688
viranmalaka <malaka.14@cse.mrt.ac.lk>
2016-12-14 21:44:08 +0530

    create controllers to seperate controlling part with router.js

--- routes.js
+++ routes.js
@@ -1,38 +1,14 @@
+const PageController = require("./src/controllers/PageController");
+const AuthController = require("./src/controllers/AuthController");
 
 module.exports = function (app, passport) {
 
-	app.get('/', function (req, res) {
-
-        res.render('index');
-    });
-
-    app.get('/login', function (req, res) {
-        res.render('login', {message: req.flash('loginMessage')});
-    });
-
-    app.get('/signup', function (req, res) {
-
-        // render the page and pass in any flash data if it exists
-        res.render('signup', {message: req.flash('signupMessage')});
-    });
-
-
-    app.get('/logout', function (req, res) {
-        req.logout();
-        res.redirect('/');
-    });
-
-    app.post('/login', passport.authenticate('local-login', {
-        successRedirect: '/', // redirect to the secure profile section
-        failureRedirect: '/login', // redirect back to the signup page if there is an error
-        failureFlash: true // allow flash messages
-    }));
-
-    app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect: '/', // redirect to the secure profile section
-        failureRedirect: '/signup', // redirect back to the signup page if there is an error
-        failureFlash: true // allow flash messages
-    }));
+	app.get('/', PageController.getIndex);
+    app.get('/login', AuthController.getLogin);
+    app.get('/signup', AuthController.getSignup);
+    app.get('/logout', AuthController.getLogout);
+    app.post('/login', AuthController.postLogin);
+    app.post('/signup', AuthController.postSignup);
 
     //dummy routes to test viwes.
     app.get("/breakdownreport", (req, res) => {res.render('breakdown/report');});
