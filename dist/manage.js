6cc9cf53ba43f71e08c8a8403bad9bf595938606
viranmalaka <malaka.14@cse.mrt.ac.lk>
2016-12-16 04:38:17 +0530

    commit before pull

--- dist/manage.js
+++ dist/manage.js
@@ -9,6 +9,7 @@ var _dbConfig = require('./db.config.js');
 var _dbConfig2 = _interopRequireDefault(_dbConfig);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+require("babel-register");
 
 require("babel-register");
 
