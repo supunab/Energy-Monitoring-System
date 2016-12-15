152ba57e53708895f402220ea4b9b98b0a885198
viranmalaka <malaka.14@cse.mrt.ac.lk>
2016-12-15 20:56:25 +0530

    done views

--- src/controllers/ComplainController.js
+++ src/controllers/ComplainController.js
@@ -1,5 +1,36 @@
+const Complain = require('./../model/Complain');
+
+
+// return all complain objects only for admins
+exports.getIndex = function (req, res) {
+    let complains = Complain.find();
+    console.log(complains);
+};
+
+
 exports.getCreateComplain = function (req, res) {
     res.render('complain/create');
 };
+
+// save complain
+exports.createComplainPOST = function (req, res) {
+
+};
+
+// return complain edit form with complain
+exports.editComplainGET = function (req, res) {
+
+};
+
+// update the complain object
+exports.editComplainPOST = function (req, res) {
+
+};
+
+// delete a complain
+exports.deletePOST = function (req, res) {
+
+}
+
 
 
--- src/model/index.js
+++ src/model/index.js
@@ -1,7 +1,11 @@
 import User from '../model/User'
+<<<<<<< ae7d5ecc990fa5d45e73665d4917edec0c09846d
 import Complaint from '../model/Complaint'
 import Area from '../model/Area'
 import Test from '../model/Test'
+=======
+import Complain from '../model/Complain'
+>>>>>>> done views
 
 export default class Migrate {
     constructor() {
