6895bfba5ba0b45dae0141c5f1c4837c6fec7ba1
viranmalaka <malaka.14@cse.mrt.ac.lk>
2016-12-15 20:56:25 +0530

    done views

--- src/model/index.js
+++ src/model/index.js
@@ -1,7 +1,9 @@
 import User from '../model/User'
+import Complain from '../model/Complain'
 
 export default class Migrate {
     constructor() {
         this.user = new User().generateSchema();
+        this.complain = new Complain().generateSchema();
     }
 }
