9f202d4b54bdda3bcccebe86ca9c74a5fecf9903
viranmalaka <malaka.14@cse.mrt.ac.lk>
2016-12-15 09:52:20 +0530

    create Connetion Controller

--- routes.js
+++ routes.js
@@ -1,19 +1,25 @@
 const PageController = require("./src/controllers/PageController");
 const AuthController = require("./src/controllers/AuthController");
+const ConnectionController = require("./src/controllers/ConnectionController");
 
 module.exports = function (app, passport) {
 
+    //pages
 	app.get('/', PageController.getIndex);
+
+	//auth routes
     app.get('/login', AuthController.getLogin);
     app.get('/signup', AuthController.getSignup);
     app.get('/logout', AuthController.getLogout);
     app.post('/login', AuthController.postLogin);
     app.post('/signup', AuthController.postSignup);
 
+    //Connection routes
+    app.get('/connection/request', ConnectionController.getRequest);
+
     //dummy routes to test viwes.
     app.get("/breakdownreport", (req, res) => {res.render('breakdown/report');});
     app.get("/breakdownupdate", (req, res) => {res.render('breakdown/update_status');});
-    app.get("/connectionrequest", (req, res) => {res.render('connection/request');});
 };
 
 
