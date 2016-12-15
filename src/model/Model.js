ae7d5ecc990fa5d45e73665d4917edec0c09846d
viranmalaka <malaka.14@cse.mrt.ac.lk>
2016-12-15 18:23:05 +0530

    add If not exists to table creating.

--- src/model/Model.js
+++ src/model/Model.js
@@ -56,7 +56,8 @@ export default class Model {
     }
 
     generateSchema() {
-        let schema = 'CREATE TABLE ' + this.constructor.name + '(';
+        let schema = 'CREATE TABLE IF NOT EXISTS' + this.constructor.name + ' (';
+
         let pk;
         let fk = [];
         let m2m = [];
