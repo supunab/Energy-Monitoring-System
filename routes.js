14fc8e2887277f18e3854dcb080ed3f2a7bf68a4
erangad <erangadulshan.14@cse.mrt.ac.lk>
2016-12-17 23:12:44 +0530

    breakdowns added to database

--- routes.js
+++ routes.js
@@ -27,7 +27,7 @@ module.exports = function (app, passport) {
     });
 
     app.post('/login', passport.authenticate('local-login', {
-        successRedirect: '/connectionRequest', // redirect to the secure profile section
+        successRedirect: '/breakdownreport', // redirect to the secure profile section
         failureRedirect: '/login', // redirect back to the signup page if there is an error
         failureFlash: true // allow flash messages
     }));
@@ -61,6 +61,8 @@ module.exports = function (app, passport) {
     app.post('/connectionRequest',ConnectionController.postRequest);
     app.get('/breakdownView',BreakDownController.getRequest);
     app.get('/paymentHistoryRegistered', (req, res) => {res.render('registeredUser/paymentHistory')});
+
+    app.post('/breakdownPost',BreakDownController.postBreakdown);
 
 };
 
