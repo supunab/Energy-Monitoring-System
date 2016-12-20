1e165771cfdb1599a31e2b594db109f9025fe090
Prabod Rathnayaka <prabod@rathnayaka.me>
2016-12-21 01:06:34 +0530

    Signup done
    Signed-off-by: Prabod Rathnayaka <prabod@rathnayaka.me>

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ var express = require('express'),
     crypto = require('crypto'),
     flash = require('connect-flash'),
     path = require('path');
-require('./src/middleware/passport')(passport);
 var app = express();
+require('./src/middleware/passport')(passport, app);
 app.use(cookieParser('energymonitor'));
 app.use(cookieSession({
     keys: ['key1', 'key2']
@@ -82,6 +82,9 @@ var handlebars = require('express-handlebars').create({
             } else {
                 return 'Not Valid';
             }
+        },
+        user: function () {
+            return app.locals.user;
         }
     }
 });
